68035cd2ccd444cd2b4ac593b6efccc709cdd84e
Michael Kjemtrup <mckjemtrup@gmail.com>
2017-04-11 13:15:56 +0200

    initial

--- Snapchat/SignInViewController.swift
+++ Snapchat/SignInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInViewController.swift
 //  Snapchat
 //
 //  Created by Michael Kjemtrup on 25/03/2017.
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func enterBtnPushed(_ sender: Any) {
     }
-
+    
 
 }
 
